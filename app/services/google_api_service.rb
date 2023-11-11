6ebf93dc9696271f5333396d06e5c5c43e50d765
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-11 17:27:07 -0600

    starter for api service

---
+++ app/services/google_api_service.rb
@@ -0,0 +1,17 @@
+class GoogleApiService
+
+  
+    def recipe_search(q)
+      get_url("/api/recipes/v2?type=public&q=#{q}&app_id=#{Rails.application.credentials.edamam[:app_id]}&app_key=#{Rails.application.credentials.edamam[:app_key]}")[:hits]
+    end
+  # 
+    def get_url(url)
+      response = connection.get(url)
+      json = JSON.parse(response.body, symbolize_names: true)
+    end
+  
+    def connection
+      Faraday.new(url: "https://api.edamam.com") 
+    end
+  end
+end
