372410e7c32aba87548097b0072c042431a2377b
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-11 13:58:48 -0600

    country facade to get all and random countries

---
+++ app/facades/country_facade.rb
@@ -0,0 +1,20 @@
+class CountryFacade
+  def initialize()
+    @service = CountryService.new
+    @country_list = all_countries
+  end
+
+  def all
+    @country_list ||= all_countries
+  end 
+
+  def random_country
+    all.sample
+  end
+
+  def all_countries
+    @service.get_all.map do |country|
+      Country.new(country)
+    end
+  end
+end
