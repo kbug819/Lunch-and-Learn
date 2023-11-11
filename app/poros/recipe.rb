c18730d034e1d1dad3a2ef6d70c837e87d64a275
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-11 14:00:13 -0600

    recipe poro added

---
+++ app/poros/recipe.rb
@@ -0,0 +1,10 @@
+class Recipe
+  attr_reader :id, :title, :url, :country, :image_url
+  def initialize(recipe_data, country)
+    @id = nil
+    @title = recipe_data[:recipe][:label]
+    @url = recipe_data[:recipe][:uri]
+    @country = country
+    @image_url = recipe_data[:recipe][:image]
+  end
+end
