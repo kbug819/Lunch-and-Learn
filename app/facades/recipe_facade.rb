4ad0097c31ac64e9755f7a9610298151a1301e74
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-11 13:59:41 -0600

    facade update for receipes to find by given or rancom country

---
+++ app/facades/recipe_facade.rb
@@ -0,0 +1,18 @@
+class RecipeFacade
+  def initialize
+    @service = RecipeService.new
+  end
+
+  def recipe_search(search_data)
+    if search_data.empty?
+      country = CountryFacade.new.random_country.name
+      recipes = @service.recipe_search(country).map do |recipe|
+        Recipe.new(recipe, country)
+      end
+    else
+      recipes = @service.recipe_search(search_data).map do |recipe|
+        Recipe.new(recipe, search_data)
+      end
+    end
+  end
+end
