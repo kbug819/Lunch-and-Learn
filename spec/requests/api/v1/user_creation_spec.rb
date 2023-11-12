03a5bec1b22ba63710155b79b3c796eacea18595
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-11 19:20:23 -0600

    test update

--- spec/requests/api/v1/user_creation_spec.rb
+++ spec/requests/api/v1/user_creation_spec.rb
@@ -3,3 +3,7 @@ require 'rails_helper'
 describe "user API" do
   describe "post sends information to create a user" do
     it "returns a list of recipes associated with a searched country" do
+      
+    end
+  end
+end
