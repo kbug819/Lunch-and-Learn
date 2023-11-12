1d496e58a0a48d4d1198dceb018f61680773db57
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-11 23:03:09 -0600

    test: user class

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe User, type: :model do
       expect(user).to validate_uniqueness_of(:email)
     end
   end
+
+  describe 'relationships' do
+    it {should have_many :favorites}
+  end
 end
