707ed169c670ee3b027e6ccb8a50e6efbe25572b
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-11 18:10:05 -0600

    service update

--- app/services/google_api_service.rb
+++ app/services/google_api_service.rb
@@ -1,17 +1,17 @@
 class GoogleApiService
 
   
-    def recipe_search(q)
-      get_url("/api/recipes/v2?type=public&q=#{q}&app_id=#{Rails.application.credentials.edamam[:app_id]}&app_key=#{Rails.application.credentials.edamam[:app_key]}")[:hits]
-    end
-  # 
-    def get_url(url)
-      response = connection.get(url)
-      json = JSON.parse(response.body, symbolize_names: true)
-    end
-  
-    def connection
-      Faraday.new(url: "https://api.edamam.com") 
-    end
+  def video_search(q)
+    get_url("/youtube/v3/search?part=snippet&channelId=UCluQ5yInbeAkkeCndNnUhpw&maxResults=1&q=#{q}&key=#{Rails.application.credentials.googleapi[:api_key]}")[:items]
+  end
+# 
+  def get_url(url)
+    response = connection.get(url)
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def connection
+    Faraday.new(url: "https://youtube.googleapis.com") 
   end
 end
+
