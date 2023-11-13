4383125ae08659392d7db210cc8680c8832177a8
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-13 10:43:20 -0600

    update to country poro to collect lat/long and capital

--- app/poros/country.rb
+++ app/poros/country.rb
@@ -1,6 +1,8 @@
 class Country
-  attr_reader :name
+  attr_reader :name, :capital, :capital_info
   def initialize(country_data)
     @name = country_data[:name][:official]
+    @capital = country_data[:capital]
+    @capital_info = country_data[:capitalInfo][:latlng]
   end
 end
