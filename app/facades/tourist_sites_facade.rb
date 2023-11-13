94d84043ed48d9aeb3465b5c9317c9b8ee5679dd
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-13 11:01:28 -0600

    tourist site facade update

--- app/facades/tourist_sites_facade.rb
+++ app/facades/tourist_sites_facade.rb
@@ -1,22 +1,14 @@
 class TouristSitesFacade
   def initialize
-    @country_service = CountryServiceService.new
+    @country_service = CountryService.new
     @places_service = PlacesService.new
   end
 
   def tourist_sites(search_data)
-      country = @country_service.by_search(search_data)
-      country_o = Country.new(country)
-      @places_service.tourist_sights(country_o.capital_info[1], country_o.capital_info[0])
-      
-
-      recipes = @service.recipe_search(country).map do |recipe|
-        Recipe.new(recipe, country)
-      
-    else
-      recipes = @service.recipe_search(search_data).map do |recipe|
-        Recipe.new(recipe, search_data)
-      end
+    country = @country_service.by_search(search_data)
+    country_o = Country.new(country)
+    sites = @places_service.tourist_sights(country_o.capital_info[1], country_o.capital_info[0]).map do |tourist_sites|
+      TouristSite.new(tourist_sites)
     end
   end
 end
