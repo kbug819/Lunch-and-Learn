3ff1a9c6a1844d5007cd24cdb48f31020a07f761
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-13 11:12:26 -0600

    tourist site controller updates

--- app/controllers/api/v1/tourist_sites_controller.rb
+++ app/controllers/api/v1/tourist_sites_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TouristSitesController < ApplicationController
   def index
     tourist_sites = TouristSitesFacade.new.tourist_sites(params[:country])
-    render json: TouristSitesSerializer.new(tourist_sites)
+    render json: TouristSiteSerializer.new(tourist_sites)
   end
 end
