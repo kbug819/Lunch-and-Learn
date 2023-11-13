af178794cfc1c01ffa00dc0efa98a736be686f69
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-13 12:16:40 -0600

    test: tourist site facade update

--- app/facades/tourist_sites_facade.rb
+++ app/facades/tourist_sites_facade.rb
@@ -7,8 +7,12 @@ class TouristSitesFacade
   def tourist_sites(search_data)
     country = @country_service.by_search(search_data)
     country_o = Country.new(country)
-    sites = @places_service.tourist_sights(country_o.capital_info[1], country_o.capital_info[0]).map do |tourist_sites|
-      TouristSite.new(tourist_sites)
+    if country_o.capital_info != nil
+      sites = @places_service.tourist_sights(country_o.capital_info[1], country_o.capital_info[0]).map do |tourist_sites|
+        TouristSite.new(tourist_sites)
+      end
+    else
+      data = []
     end
   end
 end
