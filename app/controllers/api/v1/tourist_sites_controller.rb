3780a57e42529ddc5a0d99d03c1c75f06607adeb
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-13 11:01:12 -0600

    initial code for response

--- app/controllers/api/v1/tourist_sites_controller.rb
+++ app/controllers/api/v1/tourist_sites_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TouristSitesController < ApplicationController
   def index
-    tourist_sites = TouristSitesFacade.new.sites_by_country(params[:country])
-    # render json: RecipeSerializer.new(searched_recipes)
+    tourist_sites = TouristSitesFacade.new.tourist_sites(params[:country])
+    render json: TouristSitesSerializer.new(tourist_sites)
   end
 end
