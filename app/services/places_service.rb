47e494ec32a0646c45fde7fcb8992326d67aeb52
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-13 10:40:56 -0600

    service setup for place api

--- app/services/places_service.rb
+++ app/services/places_service.rb
@@ -1,4 +1,9 @@
 class PlacesService
+  
+  def tourist_sights(long, lat)
+    get_url("/v2/places?apiKey=#{Rails.application.credentials.geoapify.api_key}&categories=tourism.sights&filter=circle:#{long},#{lat},1000")
+  end
+  
   def test_connection
     get_url("/v2/places?apiKey=#{Rails.application.credentials.geoapify.api_key}&categories=catering.restaurant&conditions=vegetarian&filter=rect:-73.99331143343392,40.77129742280923,-73.97215311088917,40.757134681430635&limit=20")
   end
