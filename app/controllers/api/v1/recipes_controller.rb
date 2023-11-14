515387d6c20627086c29baa015a5c82a2503ccf3
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-13 18:02:08 -0600

    update for first extention

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::RecipesController < ApplicationController
   def index
     searched_recipes = RecipeFacade.new.recipe_search(params[:country])
-    render json: RecipeSerializer.new(searched_recipes)
+    if searched_recipes == "No Country Found"
+      error = ErrorLogin.new("No Country Found", 'Country Incorrect', 400)
+      render json: ErrorSerializer.new(error), status: 400
+    else
+      render json: RecipeSerializer.new(searched_recipes)
+    end
   end
 end
