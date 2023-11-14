def64ecbfd25ac345794bd5a082030a04a1aed7a
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-14 12:51:39 -0600

    updated error messaging

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::LearningResourcesController < ApplicationController
   def index
     resources = LearningResourceFacade.new.by_country(params[:country])
     if resources == "No Country Found"
-      error = ErrorLogin.new("No Country Found", 'Country Incorrect', 400)
+      error = Error.new("No Country Found")
       render json: ErrorSerializer.new(error), status: 400
     else
       render json: LearningResourceSerializer.new(resources)
