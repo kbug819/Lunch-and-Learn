560c11325df397240ab394c0742fbb3ea229e477
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-14 12:09:35 -0600

    test: update for country search

--- spec/requests/api/v1/learning_resource_spec.rb
+++ spec/requests/api/v1/learning_resource_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "learning resources endpoint" do
   describe "search information for a specific country" do
     it "returns a video and a list of pictures" do
-      search_term = "Thailand"
+      search_term = "thailand"
       VCR.use_cassette("learning_resources_#{search_term}") do
 
         get "/api/v1/learning_resources?country=#{search_term}"
@@ -22,32 +22,27 @@ describe "learning resources endpoint" do
         expect(learning[:data][:attributes][:images].count).to eq 10
         expect(learning[:data][:attributes][:images][0]).to be_an Hash
         expect(learning[:data][:attributes][:images][0][:alt_tag]).to eq("standing statue and temples landmark during daytime")
-        expect(learning[:data][:attributes][:images][0][:url]).to eq("https://images.unsplash.com/photo-1528181304800-259b08848526?ixid=M3w1MDU1OTR8MHwxfHNlYXJjaHwxfHxUaGFpbGFuZHxlbnwwfHx8fDE2OTk5MDkxMTd8MA&ixlib=rb-4.0.3")
+        expect(learning[:data][:attributes][:images][0][:url]).to eq("https://images.unsplash.com/photo-1528181304800-259b08848526?ixid=M3w1MDU1OTR8MHwxfHNlYXJjaHwxfHx0aGFpbGFuZHxlbnwwfHx8fDE2OTk5ODQwOTh8MA&ixlib=rb-4.0.3")
       end
     end
 
-    it "returns an empty list  in attributes if there are no videos or images" do
+    it "returns an error if search term is not a country" do
       search_term = "nomatch"
       VCR.use_cassette("learning_resources_#{search_term}") do
 
         get "/api/v1/learning_resources?country=#{search_term}"
-        expect(response).to be_successful
+        expect(response).to_not be_successful
 
         learning = JSON.parse(response.body, symbolize_names: true)
  
-        expect(learning[:data]).to be_an Hash
-        expect(learning[:data][:id]).to eq(nil)
-        expect(learning[:data][:type]).to eq("learning_resource")
-        expect(learning[:data][:attributes][:country]).to eq("#{search_term}")
-        expect(learning[:data][:attributes][:video]).to be_a Hash
-        expect(learning[:data][:attributes][:video][:title]).to eq("A Super Quick History of Chad")
-        expect(learning[:data][:attributes][:video][:youtube_video_id]).to eq("rAKxmRI4wHI")
-        expect(learning[:data][:attributes][:images]).to eq([])
+        expect(learning[:error][:error_message]).to eq("No Country Found")
+        expect(learning[:error][:status]).to eq("Country Incorrect")
+        expect(learning[:error][:code]).to eq(400)
       end
     end
 
-    it "returns an empty list  in attributes if there are no videos or images" do
-      search_term = "thisisnotacountry"
+    it "returns an empty array or empty hash if no videos or images are returned" do
+      search_term = "northern mariana islands"
       VCR.use_cassette("learning_resources_#{search_term}") do
 
         get "/api/v1/learning_resources?country=#{search_term}"
@@ -56,9 +51,9 @@ describe "learning resources endpoint" do
         learning = JSON.parse(response.body, symbolize_names: true)
         expect(learning[:data][:id]).to eq(nil)
         expect(learning[:data][:type]).to eq("learning_resource")
-        expect(learning[:data][:attributes][:country]).to eq("thisisnotacountry")
-        expect(learning[:data][:attributes][:video]).to eq(nil)
-        expect(learning[:data][:attributes][:images]).to eq([])
+        expect(learning[:data][:attributes][:country]).to eq(search_term)
+        expect(learning[:data][:attributes][:video]).to eq({})
+        expect(learning[:data][:attributes][:images]).to be_an Array
       end
     end
   end
