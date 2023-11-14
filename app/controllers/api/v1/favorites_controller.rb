0ae3968f4ebae1f9ecab1b225ec482d1f8cc350a
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-11-14 12:51:21 -0600

    updated error messaging

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -5,8 +5,8 @@ class Api::V1::FavoritesController < ApplicationController
       favorites = user.favorites
       render json: FavoriteSerializer.new(favorites), status: :created
     else
-      error = ErrorLogin.new("Bad credentials, please try again", 'Bad Credentials', 404)
-      render json: ErrorSerializer.new(error), status: 404
+      error = Error.new("Bad credentials, please try again")
+      render json: ErrorSerializer.new(error), status: 400
     end
   end
   
@@ -17,8 +17,27 @@ class Api::V1::FavoritesController < ApplicationController
       success = Success.new("Favorite added successfully")
       render json: SuccessSerializer.new(success), status: 201
     else
-      error = ErrorLogin.new("Bad credentials, please try again", 'Bad Credentials', 404)
-      render json: ErrorSerializer.new(error), status: 404
+      error = Error.new("Bad credentials, please try again")
+      render json: ErrorSerializer.new(error), status: 400
     end
   end
+  
+  def destroy
+    user = User.find_by(api_key: params[:api_key])
+    u_favorite = Favorite.find_by(id: params[:id])
+    if user
+      if user.id == u_favorite.user_id
+        Favorite.delete(u_favorite)
+        success = Success.new("Favorite deleted")
+        render json: SuccessSerializer.new(success), status: 201
+      else
+        error = Error.new("Bad credentials, please try again")
+        render json: ErrorSerializer.new(error), status: 400
+      end
+    else
+      error = Error.new("Bad credentials, please try again")
+      render json: ErrorSerializer.new(error), status: 400
+    end
+  end
+
 end
